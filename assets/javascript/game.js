d892af0f57d3f549ab66b0abb666d1e7d8291ec9
Kate <you@example.com>
2019-03-11 20:39:18 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,9 @@ var words = ['sailormoon', 'luna', 'jupitar', "mars", "star"];
 console.log(words)
 //var word = words[Math.floor(Math.random() * words.length)];
 
-//var answerArray = [];
- //for (var i = 0; i < word.length; i++) {
- //answerArray[i] = "_";
-//}
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+answerArray[i] = "_";
+}
 
  
