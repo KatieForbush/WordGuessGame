ff290ba582670b31261608536e78bb619ac6b74a
Kate <you@example.com>
2019-03-12 21:48:25 -0700

    trying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,24 @@
-var words = ['sailormoon', 'luna', 'jupitar', "mars", "star"];
-console.log(words)
+var words = ['sailormoon', 'luna', 'jupitar', "mars", "star"];{
+
+    return word[Math.floor(math.random()*words.length]
+}
+
+<input type="test" onkeypress="letterPressed"></input>
+
+function letterPressed(){
+    //if letter pressed is correct then put it on the screen and put it in the already used area
+    var letter = (" ")
+    //if (letter = true);
+
+}
+//console.log(words)
 //var word = words[Math.floor(Math.random() * words.length)];
 
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-answerArray[i] = "_";
-}
+//var answerArray = [];
+//for (var i = 0; i < word.length; i++) {
+//answerArray[i] = "_";
+//}
+
+
 
  
