c280994e881771623105f9d0cd3ce36c7302e0fd
Kate <you@example.com>
2019-03-13 14:37:21 -0700

    getting there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,32 @@
-var words = ['sailormoon', 'luna', 'jupitar', "mars", "star"];{
+window.onload = function () {
 
-    return word[Math.floor(math.random()*words.length]
+var word ;              // Selected word
+var guess ;             // Geuss
+var geusses = [ ];      // Stored geusses
+var lives ;             // Lives
+var counter ;           // Count correct geusses
+var space;              // Number of spaces in word
+
+//<input type="text" onkeypress="myFunction()"></input>
 }
 
-<input type="test" onkeypress="letterPressed"></input>
+// Show lives
+comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+var words = ["galaxy", "heart", "coffee", "travel"];
+function secretWord()
+    return secretWord[Math.floor(math.random()*words.length]
+
+//<input type="test" onkeypress="letterPressed"></input>
 
 function letterPressed(){
     //if letter pressed is correct then put it on the screen and put it in the already used area
@@ -18,6 +41,17 @@ function letterPressed(){
 //for (var i = 0; i < word.length; i++) {
 //answerArray[i] = "_";
 //}
+
+
+// Reset
+
+//document.getElementById('reset').onclick = function() {
+    //correct.parentNode.removeChild(correct);
+    //letters.parentNode.removeChild(letters);
+    //showClue.innerHTML = "";
+    //context.clearRect(0, 0, 400, 400);
+    //play();
+  //}
 
 
 
